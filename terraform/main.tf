272ff9a988a5cd7232e6588b941d1df52109d503
Bala <ksbalamugunthan@outlook.com>
2026-03-10 12:04:16 +0000

    Add Terraform infra pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,193 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name   = "alb-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "bala-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb" "app_alb" {
+  name               = "bala-app-alb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb_sg.id]
+
+  tags = {
+  Project = "bala-flask-app"
+}
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+resource "aws_launch_template" "app_lt" {
+  name_prefix   = "bala-app"
+  image_id      = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.ec2_profile.arn
+  }
+
+user_data = base64encode(<<EOF
+#!/bin/bash
+yum update -y
+yum install -y docker aws-cli
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+
+aws ecr get-login-password --region ap-south-1 | \
+docker login --username AWS --password-stdin 443427140918.dkr.ecr.ap-south-1.amazonaws.com
+
+docker pull 443427140918.dkr.ecr.ap-south-1.amazonaws.com/bala-flask-app:latest
+
+docker run -d -p 80:5000 \
+443427140918.dkr.ecr.ap-south-1.amazonaws.com/bala-flask-app:latest
+EOF
+  )
+}
+
+resource "aws_autoscaling_group" "app_asg" {
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  launch_template {
+    id      = aws_launch_template.app_lt.id
+    version = "$Latest"
+  }
+
+  instance_refresh {
+  strategy = "Rolling"
+}
+
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
+}
+
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ssm-ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-ssm-ecr-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "bala-terraform-state-bucket"
+    key    = "flask-app/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+resource "aws_autoscaling_group" "app_asg" {
+
+  health_check_type = "ELB"
+  health_check_grace_period = 300
+
+}
